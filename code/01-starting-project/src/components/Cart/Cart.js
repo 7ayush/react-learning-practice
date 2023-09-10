705127b40a0d769b9a61e5195c6fc14eeb309ebd
7ayush <ayush.kaushik711@gmail.com>
2023-09-11 01:07:49 +0530

    Create Modal

--- code/01-starting-project/src/components/Cart/Cart.js
+++ code/01-starting-project/src/components/Cart/Cart.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
 
 const Cart = (props) => {
     const cartItems = <ul>
         {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(item => {
-            return <li>item.name</li>
+            return <li>{item.name}</li>
         })}
     </ul>
-    return <div>
+    return <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -17,7 +18,7 @@ const Cart = (props) => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
 
 }
 
