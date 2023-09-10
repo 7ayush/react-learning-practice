2b902a4a452b05f68fafd0be338502c8fdfb7259
7ayush <ayush.kaushik711@gmail.com>
2023-09-11 00:03:54 +0530

    Adding MealItem using Card and props

--- code/01-starting-project/src/components/Meals/AvailableMeals.js
+++ code/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import DUMMY_MEALS from '../Constants/dummy-meals'
 import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal =>{
-        return <li>{meal.name}</li>
+    const mealsList = DUMMY_MEALS.map(meal => {
+        return <MealItem
+            key={meal.id}
+            name={meal.name}
+            price={meal.price}
+            description={meal.description}
+        />
     })
+    
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList} 
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
