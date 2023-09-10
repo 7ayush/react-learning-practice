a28d23b691301c79ee350ce1984af8cfd32c68c4
7ayush <ayush.kaushik711@gmail.com>
2023-09-10 18:36:55 +0530

    Creating Meal component structure

---
+++ code/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import DUMMY_MEALS from '../Constants/dummy-meals'
+import classes from './AvailableMeals.module.css'
+
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal =>{
+        return <li>{meal.name}</li>
+    })
+    return (
+        <section className={classes.meals}>
+            <ul>
+                {mealsList} 
+            </ul>
+        </section>
+    )
+}
+
+export default AvailableMeals
