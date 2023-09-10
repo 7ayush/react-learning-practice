39b3ec1b204f27ff181ddb319fb8eea795b6da76
7ayush <ayush.kaushik711@gmail.com>
2023-09-11 01:18:08 +0530

    Add Opening closing feature for HP rich

--- code/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ code/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -2,11 +2,11 @@ import React from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span>
-                <CartIcon className={classes.icon}/>
+                <CartIcon className={classes.icon} />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>3</span>
